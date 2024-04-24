688e5a6295415e2900638e116a38042e33301b0d
Ashside <maybeyushu@outlook.com>
2024-04-24 08:18:15 +0800

    修改测试脚本

--- Lab/code/test.sh
+++ Lab/code/test.sh
@@ -1,3 +1,8 @@
 make clean
 make
-./parser ../testcases/input5.cmm
+# 读入一个参数
+echo "------------------------------------------------------"
+echo "Running Testcase $1"
+./parser ../testcases/input$1.cmm
+echo "------------------------------------------------------"
+make clean
